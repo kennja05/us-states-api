5d2906838762d60508e4d7445fec0d38e8b0264a
github username <github email address>
2020-03-03 12:44:08 -0500

    post to /games added

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   
 
   get '/games', to: 'games#index', as: 'games'
+
+  post '/games', to: 'games#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
