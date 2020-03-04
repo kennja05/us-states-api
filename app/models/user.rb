a35b6749b220c476fbeb617df4ecadd8e26cbcc8
kennja05 <jacobkenny05@gmail.com>
2020-03-04 09:44:27 -0500

    added default profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
 
     validates :name, uniqueness: true 
     validates :password, presence: true
-    validates :image, presence: true 
+    #validates :image, presence: true 
+    before_create :default_values
+
+    
+    def default_values
+        if self.image == nil || self.image == ""
+            self.image = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+        end 
+    end
+    
+
+
 end
